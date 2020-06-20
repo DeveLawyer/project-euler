752e8bf454a8bdbc565077634a819952d2e9d15a
Josue Narro <josue.narro.a@gmail.com>
2020-06-19 22:37:25 -0500

    Fix variable i

--- problem001.js
+++ problem001.js
@@ -4,7 +4,7 @@
 
 function sumMultiples3or5(target) {
   let sum = 0;
-  for (let i = 0; i < target; i++) {
+  for (let i = 1; i < target; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
       sum += i;
     }
